e25f47849e73055bfe75ba91bc4cacb4334c2a62
wnoune <wilfriednoune3@gmail.com>
2022-05-09 14:09:11 +0100

    Update

--- src/tests/step_definitions/AuthStep.js
+++ src/tests/step_definitions/AuthStep.js
@@ -1,2 +1,14 @@
-import AuthPage from '../pages/AuthPage';
-import {And, Given, Then, When} from "cypress-cucumber-processor/steps";
+import {And, Given, Then, When} from "cypress-cucumber-preprocessor/steps"
+import AuthPage from '../Pages/AuthPage';
+
+//TC_807
+//Login and password field required
+
+    When ('I click on connexion button without filling in the login and password fields', () =>{
+        AuthPage.clickOnLoginBtn()
+    });
+
+    Then('User should see and error message below the login and password fields', ()=> {
+        cy.contains('email est invalid')
+        .should('be.visible')
+    });
